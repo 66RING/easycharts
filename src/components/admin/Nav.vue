907248e6d9cd7046e66554a203bcc40e99a2431f
66RING <travisturing@qq.com>
2022-04-05 20:39:16 +0800

    使用element UI的布局，移除ant design

--- src/components/admin/Nav.vue
+++ src/components/admin/Nav.vue
@@ -1,30 +1,48 @@
 <template>
-  <a-layout-sider breakpoint="lg" v-model="collapsed">
-    <div class="log">
-      <span>{{collapsed? "EC" : "EasyCharts"}}</span>
-    </div>
+  <!-- 侧边栏 -->
+  <el-aside width="200px">
+	<!-- 侧边栏菜单区域 -->
+	<el-menu
+	  router
+	  active-text-color="#ffd04b"
+	  background-color="#545c64"
+	  text-color="#fff">
+	  <!-- 菜单 -->
+	  <!-- 示例页面 -->
+	  <el-menu-item index="/demo">
+	  <template #title>
+		<el-icon><document/></el-icon>
+		<span>示例</span>
+	  </template>
+	  </el-menu-item>
 
-    <a-menu theme="dark" mode="inline" @click="goToPage">
+	  <!-- 制作图表页面 -->
+	  <el-menu-item index="/MakeCharts">
+	  <template #title>
+		<el-icon><document/></el-icon>
+		<span>图表</span>
+	  </template>
+	  </el-menu-item>
 
-      <a-menu-item key="CodeEditor">
-        <ToolOutlined /><span>代码编辑</span></a-menu-item>
-      <a-menu-item key="Demo">
-        <ToolOutlined /><span>查看示例</span></a-menu-item>
-      <a-menu-item key="MakeCharts">
-        <ToolOutlined /><span>制作图表</span></a-menu-item>
-    </a-menu>
+	  <!-- 代码编译页面 -->
+	  <el-menu-item index="/CodeEditor">
+	  <template #title>
+		<el-icon><document/></el-icon>
+		<span>编辑</span>
+	  </template>
+	  </el-menu-item>
 
-  </a-layout-sider>
+	</el-menu>
+  </el-aside>
 </template>
 
 <script>
-import { 
-  ToolOutlined
-} from '@ant-design/icons-vue';
+
+import { Document } from '@element-plus/icons-vue'
 
 export default {
   components: {
-	ToolOutlined,
+	Document,
   },
   data() {
     return {
@@ -32,9 +50,10 @@ export default {
     }
   },
   methods: {
-    goToPage(item) {
-      this.$router.push('/'+item.key).catch((err) => err)
-    }
+    // goToPage(item) {
+	//   console.log(item)
+    //   //this.$router.push('/'+item.key).catch((err) => err)
+    // }
   }
 }
 </script>
@@ -50,4 +69,12 @@ export default {
   align-items: center;
   font-size: 17px;
 }
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
 
 import { router } from './router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
 const app = createApp(App)
 app.use(router)
-app.use(Antd)
+app.use(ElementPlus)
 app.mount('#app')
